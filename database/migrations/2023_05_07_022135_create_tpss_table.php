f54454fa64fd75a12eb47c77621afa291a98af95
hanifnabila <hanifnabila10@gmail.com>
2023-05-07 09:30:58 +0700

    add : create model & migration tps

---
+++ database/migrations/2023_05_07_022135_create_tpss_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tpss', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_tps');
+            $table->decimal('longitude', 10, 7);
+            $table->decimal('latitude', 10, 7);
+            $table->string('alamat');
+            $table->integer('volume');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tpss');
+    }
+};
