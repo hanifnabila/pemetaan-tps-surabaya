517c3307a8a7833733093bd9331c9615888533f6
hanifnabila <hanifnabila10@gmail.com>
2023-05-17 13:57:20 +0700

    delete : view tps

--- routes/web.php
+++ routes/web.php
@@ -2,20 +2,7 @@
 
 use App\Models\Magang;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TpsController;
 use App\Http\Controllers\MagangController;
-use App\Http\Controllers\MapsController;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -31,7 +18,3 @@ Route::get('/maps', function () {
     return view('maps');
 });
 Route::apiResource('magang', MagangController::class);
-
-// Route::get('/geojson-data', 'GeoJSONController@getData');
-
-Route::apiResource('maps', MapsController::class);
